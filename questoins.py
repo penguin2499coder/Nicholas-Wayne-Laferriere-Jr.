8308d2a793ac04d525423fdca9df39a737357a9c
penguin2499coder <penguin2499coder@penguincoding.org>
2024-07-29 14:50:40 -0400

    Add files via upload

---
+++ questoins.py
@@ -0,0 +1,16 @@
+questoin1=input("What state has the most water?")
+if questoin1=="Florida":
+    print("Correct, Florida has the most water.")
+else:
+    print("Incorrect, Florida has the most water.")
+questoin2=input("Okay next questoin, what is the hardest game on the intrnet?")
+if questoin2=="The hardest game":
+    print("Correct, that is the hardest game on the internt and lives up to it's name.")
+else:
+    print("Incorrect, the hardest game on the internt it's called, the hardest game and lives up to it's name")
+questoin3=input("Where is the biggest sidewalk in the world?")
+if questoin3=="Tampa, Florida":
+    print("Correct, that is where the longest sidewalk presents.")
+else:
+    print("Incorrect, the longest sidewalk in the world is in Tampa, Florida")
+    print("I hoped you enjoyed my questoins of the world page! The end.")
